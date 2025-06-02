28b4bcdd0c40afc484659d15dbdd4a59cce5dadc
wyt_cpri <83196369+junwooYeom@users.noreply.github.com>
2025-06-02 09:27:40 +0900

    Modify Compile Error

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { ReactNode } from 'react'
 
 export default function AdminLayout({
@@ -15,18 +16,18 @@ export default function AdminLayout({
                 <h1 className="text-xl font-bold">Admin Dashboard</h1>
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                <a
+                <Link
                   href="/admin/users"
                   className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 >
                   유저 관리
-                </a>
-                <a
+                </Link>
+                <Link
                   href="/admin/properties"
                   className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 >
                   매물 관리
-                </a>
+                </Link>
               </div>
             </div>
           </div>
